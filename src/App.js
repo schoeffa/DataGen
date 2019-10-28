fc441129bb8995313362030aebd0e87b67ce10ed
aaron <schoeffa@gmail.com>
2019-10-28 00:00:31 -0400

    Began to build logic for producing a user inputted number of rows of data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import companies from './companies.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      rows: 0
+    };
+  }
+
+  handleRowInput = (e) => {
+    this.setState({rows: e.target.value});
+  }
+
+  handleSubmit = () => {
+    let data = [];
+    let coverageTypes = ['Workers Compensation', 'General Liability', 'Commercial Auto', 'Property']
+    let company = companies[Math.floor(Math.random() * companies.length - 1)];
+    for(let i=0; i<this.state.rows; i++) {
+      let row = {
+        company: company,
+        coverage: coverageTypes[Math.floor(Math.random() * coverageTypes.length - 1)];
+      }
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Rows</label><input type='number' value={this.state.rows} onChange={this.handleRowInput}></input>
+        <button >Generate Data</button>
+      </form>
+    );
+  }
 }
 
 export default App;
