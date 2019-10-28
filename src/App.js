7f7416688b66849bca8521e1a3a255643d3f30c5
aaron <schoeffa@gmail.com>
2019-10-28 10:38:53 -0400

    Successfully generates a user defined number of rows of data with randomized claim numbers, coverage types, and loss types; transform object into a csv and downloads

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Papa from 'papaparse';
 import './App.css';
 import companies from './companies.json';
+import coverages from './coveragetypes.json'
 
 export class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      rows: 0
+      rows: 0,
+      data: []
     };
   }
 
@@ -14,16 +17,43 @@ export class App extends React.Component {
     this.setState({rows: e.target.value});
   }
 
-  handleSubmit = () => {
-    let data = [];
-    let coverageTypes = ['Workers Compensation', 'General Liability', 'Commercial Auto', 'Property']
-    let company = companies[Math.floor(Math.random() * companies.length - 1)];
-    for(let i=0; i<this.state.rows; i++) {
-      let row = {
-        company: company,
-        coverage: coverageTypes[Math.floor(Math.random() * coverageTypes.length - 1)];
-      }
+  generateClaimNum = () => {
+    let chars = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let claimNumber = '';
+    for (let i=0; i<7; i++)  {
+      claimNumber += chars[Math.floor(Math.random() * chars.length)];
     }
+    return claimNumber;
+  }
+
+  outputCSV = (data) => {
+    let csv = Papa.unparse(data);
+    let filename = 'dataset.csv';
+    csv = 'data:text/csv;charset=utf-8, ' + csv;
+    let dataset = encodeURI(csv);
+    let link = document.createElement('a');
+        link.setAttribute('href', dataset);
+        link.setAttribute('download', filename);
+        link.click();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let data = [];
+    let company = companies[Math.floor(Math.random() * companies.length)];
+    
+    for(let i=0; i<this.state.rows; i++) {
+      let coverageKey = Math.floor(Math.random() * coverages.length);
+      let row = {
+        insuredName: company.name,
+        claimNumber: this.generateClaimNum(),
+        coverageType: coverages[coverageKey].coverageType,
+        lossType: coverages[coverageKey].lossTypes[Math.floor(Math.random() * coverages[coverageKey].lossTypes.length)]
+      };
+      data.push(row);
+    }
+    console.log(data);
+    this.outputCSV(data);
   }
 
   render() {
